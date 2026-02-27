df79e2fd41f0212ccff3c401dea9e1a7aa51dbe5
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2026-02-27 15:03:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,27 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pinterest_downloader",
+    name="pinterest-downloader",
     version="2.0.6",
-    author="Ahmed Negm",
-    author_email="a7mednegm.x@gmail.com",
-    description="A Pinterest media downloader",
+    author="Ahmed Nagm",
+    description="Pinterest data extractor library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/x7007x/pinterest-downloader",
+    url="https://github.com/x7007x/PinterestDownloader",
+    project_urls={
+        "Bug Tracker": "https://github.com/x7007x/PinterestDownloader/issues",
+        "PyPI": "https://pypi.org/project/pinterest-downloader/",
+    },
     packages=find_packages(),
-    install_requires=[
-        "aiohttp"
-    ],
+    python_requires=">=3.8",
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Video",
     ],
-    python_requires=">=3.7",
+    keywords="pinterest downloader scraper extractor pin video image gif board user",
 )
